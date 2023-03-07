41b3099a8c57352f93341f887a966ddcc0b8709a
Piotrek <piotrflorek@op.pl>
2023-03-07 22:49:41 +0100

    fix for js file

--- src/pages/ReplaceMePage/componentPage.tsx
+++ src/pages/ReplaceMePage/componentPage.tsx
@@ -1,7 +1,22 @@
 import "./replaceMe.css";
-import "./replaceMe.jsx";
 
 const ReplaceMePage = () => {
+  document.addEventListener("DOMContentLoaded", () => {
+    window.onload = function () {
+      //copy here your js
+      const generateBtn = document.getElementById("generate-btn");
+      const resultEl = document.getElementById("result");
+      if (generateBtn && resultEl) {
+        generateBtn.addEventListener("click", () => {
+          const randomNumber = Math.floor(Math.random() * 100) + 1;
+          resultEl.innerText = `Your random number is: ${randomNumber}`;
+        });
+      } else {
+        console.log("One or both elements not found");
+      }
+    };
+    //end of copy
+  });
   const htmlContent = `
   <!DOCTYPE html>
   <html>
